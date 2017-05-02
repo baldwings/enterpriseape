c01d34662d7e4b3f5f7c2a4cc48528903ffe0fc3
Maurizio Mars <maurizio.9cento@gmail.com>
2017-05-02 08:57:50 +0000

    added bootstrap

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,16 @@
+class WelcomeController < ApplicationController
+  def index
+  end
+
+  def about
+  end
+
+  def faq
+  end
+
+  def pricing
+  end
+
+  def feature
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
+  get 'welcome/about'
+
+  get 'welcome/faq'
+
+  get 'welcome/pricing'
+
+  get 'welcome/feature'
+
   resources :invoices
+  
+  root 'welcome#index'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
